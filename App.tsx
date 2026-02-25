7400ac5151cfcf2bc2efee6aa810253aac94f154
kyle-qi <qijialuabc@gmail.com>
2026-02-26 01:30:52 +0800

    feat: Switch from HashRouter to BrowserRouter for clean URLs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Archive from './pages/Archive';
